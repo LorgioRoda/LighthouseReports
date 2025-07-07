0db7c5ee93c0fe34361127bec56c27b12b120261
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-07-07 15:07:03 +0200

    Update Lighthouse configuration to include desktop-specific settings for screen emulation and ensure proper report output directory structure.

--- .lighthouserc.desktop.cjs
+++ .lighthouserc.desktop.cjs
@@ -1,14 +1,20 @@
 module.exports = {
-    ci: {
-      collect: {
-        url: [
-          'https://www.cupraofficial.dk/carshop/w/model',
-        ],
-        numberOfRuns: 3,
-      },
-      upload: {
-        target: 'filesystem',
-        outputDir: './.lighthouse-reports/desktop',
+  ci: {
+    collect: {
+      url: [
+        'https://www.cupraofficial.dk/carshop/w/model',
+      ],
+      numberOfRuns: 3,
+      settings: {
+        formFactor: 'desktop',
+        screenEmulation: {
+          mobile: false,
+        },
       },
     },
-  };
+    upload: {
+      target: 'filesystem',
+      outputDir: './.lighthouse-reports/desktop',
+    },
+  },
+};
