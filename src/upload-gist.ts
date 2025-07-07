72e36e1c6c4066faf3181cfce141a098d9a826b0
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-07-07 12:54:37 +0200

    Update manifest file path in readManifest method to reflect new directory structure

--- src/upload-gist.ts
+++ src/upload-gist.ts
@@ -32,7 +32,7 @@ class LighthouseGistUploader {
 
   private readManifest(): ManifestRun[] {
     try {
-      const manifestContent = fs.readFileSync("./.lighthouseci/manifest.json", "utf-8");
+      const manifestContent = fs.readFileSync("./.lighthouse-reports/manifest.json", "utf-8");
       return JSON.parse(manifestContent);
     } catch (err) {
       console.error("❌ Unable to read manifest.json:", err);
