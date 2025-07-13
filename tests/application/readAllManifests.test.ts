1bf20ca4dc36ce890b0d4cd0f2bcfb7850e649c2
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-07-13 13:33:30 +0200

    Enhance tests for readAllManifests to validate mobile, desktop, and main sources

--- tests/application/readAllManifests.test.ts
+++ tests/application/readAllManifests.test.ts
@@ -38,12 +38,40 @@ describe("readAllManifests", () => {
     jest.clearAllMocks();
   });
     
-    it("should read all manifests", () => {
+    it("should read all manifests, mobile, desktop, and main", () => {
       const uploader = new LighthouseGistUploader('token');
       const source = (uploader as any).readAllManifests();
       expect(fs.readFileSync).toHaveBeenCalledWith(
         './.lighthouse-reports/manifest.json', 'utf-8'
       );
       expect(fs.readFileSync).toHaveBeenCalledTimes(3);
+      expect(source).toBeDefined();
+      expect(source.length).toBe(3);
+  });
+  it("should find main source", () => {
+    const uploader = new LighthouseGistUploader('token');
+    const source = (uploader as any).readAllManifests();
+    expect(fs.readFileSync).toHaveBeenCalledWith(
+      './.lighthouse-reports/manifest.json', 'utf-8'
+    );
+    expect(source[0].type).toBe('main');
+  });
+  it("should find mobile source in main manifest", () => {
+    const uploader = new LighthouseGistUploader('token');
+    const source = (uploader as any).readAllManifests();
+    expect(fs.readFileSync).toHaveBeenCalledWith(
+      './.lighthouse-reports/manifest.json', 'utf-8'
+    );
+    expect(source[0].runs[1].type).toBe('mobile');
+    expect(source[0].runs[1].path).toBe('./.lighthouse-reports/mobile/manifest.json');
+  });
+  it("should find desktop source in main manifest", () => {
+    const uploader = new LighthouseGistUploader('token');
+    const source = (uploader as any).readAllManifests();
+    expect(fs.readFileSync).toHaveBeenCalledWith(
+      './.lighthouse-reports/manifest.json', 'utf-8'
+    );
+    expect(source[0].runs[0].type).toBe('desktop');
+    expect(source[0].runs[0].path).toBe('./.lighthouse-reports/desktop/manifest.json');
   });
 });
