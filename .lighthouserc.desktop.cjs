7d1c03408f2d1aff2efe0a38619683e858fff707
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-09-01 15:05:35 +0200

    Update URLs in Lighthouse configuration files for desktop and mobile, swapping the Volkswagen link with the Toyota link for improved consistency.

--- .lighthouserc.desktop.cjs
+++ .lighthouserc.desktop.cjs
@@ -6,8 +6,8 @@ module.exports = {
           // 'https://www.audi.es/es/modelos/',
           // 'https://configurator.porsche.com/es-ES/model-start',
           // 'https://cc.skoda-auto.com/esp/es-ES/',
-          'https://www.volkswagen.es/es/configurador.html',
-          // 'https://www.toyota.es/coches',
+          // 'https://www.volkswagen.es/es/configurador.html',
+          'https://www.toyota.es/coches',
           // 'https://www.ford.es/turismos?intcmp=Build-Your-Ford',
           // 'https://www.mercedes-benz.es/passengercars/models.html'
       ],
