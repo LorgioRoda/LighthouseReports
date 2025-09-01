ce0a0af1f05d3f606ad839bf19a8d59e2a35f598
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-09-01 14:35:07 +0200

    Comment out unused URLs in Lighthouse configuration files for desktop and mobile, improving clarity and maintainability.

--- .lighthouserc.desktop.cjs
+++ .lighthouserc.desktop.cjs
@@ -2,14 +2,14 @@ module.exports = {
   ci: {
     collect: {
       url: [
-          'https://www.cupraofficial.dk/carshop/w/model',
+          // 'https://www.cupraofficial.dk/carshop/w/model',
           'https://www.audi.es/es/modelos/',
-          'https://configurator.porsche.com/es-ES/model-start',
-          'https://cc.skoda-auto.com/esp/es-ES/',
-          'https://www.volkswagen.es/es/configurador.html',
-          'https://www.toyota.es/coches',
-          'https://www.ford.es/turismos?intcmp=Build-Your-Ford',
-          'https://www.mercedes-benz.es/passengercars/models.html'
+          // 'https://configurator.porsche.com/es-ES/model-start',
+          // 'https://cc.skoda-auto.com/esp/es-ES/',
+          // 'https://www.volkswagen.es/es/configurador.html',
+          // 'https://www.toyota.es/coches',
+          // 'https://www.ford.es/turismos?intcmp=Build-Your-Ford',
+          // 'https://www.mercedes-benz.es/passengercars/models.html'
       ],
       numberOfRuns: 3,
       settings: {
