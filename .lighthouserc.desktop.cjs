acd2b4212e9beb25ac5548efca0880a1870c7757
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-09-02 12:43:12 +0200

    Update URLs in Lighthouse configuration files for desktop and mobile, replacing the Tesla link with a new Cupra link to ensure consistency across both setups.

--- .lighthouserc.desktop.cjs
+++ .lighthouserc.desktop.cjs
@@ -2,8 +2,7 @@ module.exports = {
   ci: {
     collect: {
       url: [
-          // 'https://www.cupraofficial.dk/carshop/w/model',
-          'https://www.tesla.com/es_es/model3/design#overview'
+          'https://www.cupraofficial.es/carshop/w/model?wf=cupra-es-es&id=ePl9IIpVw_Cws2OQj6efq663b4ATiucdfXzAgn4R-gawaMYSGo6Bun4GPrqBho5AeTdDEwWjSKNIENPUTAEEHAPxhBcxmmsB9lUwvA&carline=CARLINE-GROUP-KMP'
       ],
       numberOfRuns: 3,
       settings: {
